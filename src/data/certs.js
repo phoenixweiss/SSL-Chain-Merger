b5892dc173526beb322b1b6d43ffd7a6ab5cda0c
PAVEL TKACHEV <phoenixweiss@ya.ru>
2024-02-24 23:31:43 +0300

    upd certs placeholders

--- src/data/certs.js
+++ src/data/certs.js
@@ -1,46 +1,46 @@
 export const certsData = [
   {
     id: 'cert_domain',
-    type: 'domainCert',
+    type: 'cert',
     title: 'Domain certificate',
     name: 'cert_domain',
-    placeholder: 'Enter domain certificate',
+    placeholder: '-----BEGIN CERTIFICATE-----\nEnter domain certificate\n-----END CERTIFICATE-----',
     format: 'crt',
     required: true
   },
   {
     id: 'cert_inter',
-    type: 'intermediateCA',
+    type: 'cert',
     title: 'Intermediate CA certificate',
     name: 'cert_inter',
-    placeholder: 'Enter intermediate CA certificate',
+    placeholder: '-----BEGIN CERTIFICATE-----\nEnter intermediate CA certificate\n-----END CERTIFICATE-----',
     format: 'crt',
     required: false
   },
   {
     id: 'cert_root',
-    type: 'rootCA',
+    type: 'cert',
     title: 'Root CA certificate',
     name: 'cert_root',
-    placeholder: 'Enter root CA certificate',
+    placeholder: '-----BEGIN CERTIFICATE-----\nEnter root CA certificate\n-----END CERTIFICATE-----',
     format: 'crt',
     required: true
   },
   {
     id: 'private_key',
-    type: 'privateKey',
+    type: 'key',
     title: 'Private key',
     name: 'private_key',
-    placeholder: 'Enter private key',
+    placeholder: '-----BEGIN RSA PRIVATE KEY-----\nEnter private key\n-----END RSA PRIVATE KEY-----',
     format: 'key',
     required: true
   },
   {
     id: 'csr_code',
-    type: 'csrCode',
+    type: 'csr',
     title: 'Certificate signing request',
     name: 'csr_code',
-    placeholder: 'Enter certificate signing request',
+    placeholder: '-----BEGIN CERTIFICATE REQUEST-----\nEnter certificate signing request\n-----END CERTIFICATE REQUEST-----',
     format: 'csr',
     required: false
   }
