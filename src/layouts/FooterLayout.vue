6b89b7699a3040c2fa80f97e6044f2b949a65b94
PAVEL TKACHEV <phoenixweiss@ya.ru>
2024-02-24 23:51:18 +0300

    footer upd

--- src/layouts/FooterLayout.vue
+++ src/layouts/FooterLayout.vue
@@ -3,11 +3,12 @@
 <template>
   <footer>
     <small>
-      © Built with
+      &copy; {{ new Date().getFullYear() }} Made with
       <a href="https://vuejs.org/" target="_blank">Vue</a>,
       <a href="https://picocss.com/" target="_blank">Pico</a>,
-      <a href="https://sass-lang.com/" target="_blank">Sass</a>
-      and ❤️
+      <a href="https://sass-lang.com/" target="_blank">Sass</a>,
+      <a href="https://pinia.vuejs.org/" target="_blank">Pinia</a>
+      and ❤️ by <a href="https://phoenixweiss.me" target="_blank"><em>phoenixweiss</em></a>.
     </small>
   </footer>
 </template>
