f12c861b3bde39b946888996ec838f7a6dcb8850
PAVEL TKACHEV <phoenixweiss@ya.ru>
2024-09-14 18:06:32 +0300

    fix

--- src/layouts/FooterLayout.vue
+++ src/layouts/FooterLayout.vue
@@ -3,13 +3,13 @@
 <template>
   <footer>
     <small>
-      &copy; {{ new Date().getFullYear() }} {{ $t('ui.footer_made_with') }}
+      &copy; {{ new Date().getFullYear() }} {{ $t('ui.footer.made_with') }}
       <a href="https://vuejs.org/" target="_blank">Vue</a>,
       <a href="https://picocss.com/" target="_blank">Pico</a>,
       <a href="https://sass-lang.com/" target="_blank">Sass</a>,
       <a href="https://pinia.vuejs.org/" target="_blank">Pinia</a>,
       <a href="https://stuk.github.io/jszip/" target="_blank">JSZip</a>
-      {{ $t('ui.footer_made_by') }}
+      {{ $t('ui.footer.made_by') }}
       <a href="https://phoenixweiss.me" target="_blank"><em>phoenixweiss</em></a
       >.
     </small>
